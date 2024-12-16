bb5edc3484603649e90ebefcc05962c405902fd2
konstu <konstantin.unterweger@tuwien.ac.at>
2024-12-16 17:50:47 +0100

    should be 25/125

--- src/main/java/dslab/broker/Election.java
+++ src/main/java/dslab/broker/Election.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -30,6 +30,7 @@ public class Election implements Runnable {
     private int leaderId;
 
     private ServerSocket serverSocket;
+
     public Election(BrokerConfig config) {
         this.config = config;
         socketList = new ArrayList<>();
@@ -63,9 +64,14 @@ public class Election implements Runnable {
                     election = new ElectionHandler(config, this, socket);
                     executor.submit(election);
                 } catch (IOException e) {
-
+                    e.printStackTrace();
+                    if (serverSocket.isClosed()){
+                        System.out.println("Server socket closed");
+                        return;
+                    }
                     switch (config.electionType()) {
                         case "ring":
+                            System.err.println("from line 73");
                             ringElection(-1);
                             break;
                         case "bully":
@@ -79,6 +85,7 @@ public class Election implements Runnable {
                     }
                 }
             }
+            System.out.println("finished");
         } catch (IOException e) {
             throw new RuntimeException("Failed to set up server socket", e);
         }
@@ -86,21 +93,38 @@ public class Election implements Runnable {
 
 
     protected void ringElection(int id) {
-        if (nextSocket == null) {
-            try {
-                nextSocket = new Socket(config.electionPeerHosts()[0], config.electionPeerPorts()[0]);
-                BufferedReader in = new BufferedReader(new InputStreamReader(nextSocket.getInputStream()));
-                if (in.readLine().equals("ok LEP")) {
-                    write(nextSocket.getOutputStream(), "ok\n");
+        if (!running) {
+            return;
+        }
+        if (serverSocket != null && serverSocket.isClosed()) {
+            return;
+        }
+        System.out.println("Election: " + id);
+        if ((nextSocket == null || nextSocket.isClosed())) {
+//            System.out.println("Connecting to next peer");
+            for (int i = 0; i < config.electionPeerIds().length; i++) {
+                try {
+                    nextSocket = new Socket(config.electionPeerHosts()[i], config.electionPeerPorts()[i]);
+                    nextSocket.setSoTimeout((int) config.electionHeartbeatTimeoutMs());
+                    BufferedReader in = new BufferedReader(new InputStreamReader(nextSocket.getInputStream()));
+                    if (in.readLine().equals("ok LEP")) {
+                        System.out.println("Connected to next peer " + nextSocket);
+                        write(nextSocket.getOutputStream(), "ok\n");
+//                        System.out.println("Connected to next peer");
+                        break;
+                    }
+                } catch (IOException e) {
+                    continue;
+                    //throw new RuntimeException(e);
                 }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
             }
         }
+        System.out.println("going to elect " + id);
         if (id > config.electionId()) {
             try {
                 //Socket socket = new Socket(config.electionPeerHosts()[0], config.electionPeerPorts()[0]);
                 write(nextSocket.getOutputStream(), "elect " + id + "\n");
+                //System.out.println("elect " + id);
             } catch (IOException e) {
                 System.out.println("Error sending election message");
                 throw new RuntimeException(e);
@@ -110,7 +134,9 @@ public class Election implements Runnable {
             try {
                 //Socket socket = new Socket(config.electionPeerHosts()[0], config.electionPeerPorts()[0]);
                 write(nextSocket.getOutputStream(), "declare " + id + "\n");
+                //System.out.println("elect " + id);
             } catch (IOException e) {
+                System.out.println("Error sending election message");
                 throw new RuntimeException(e);
             }
         }
@@ -118,36 +144,20 @@ public class Election implements Runnable {
             try {
                 //Socket socket = new Socket(config.electionPeerHosts()[0], config.electionPeerPorts()[0]);
                 write(nextSocket.getOutputStream(), "elect " + config.electionId() + "\n");
+                //System.out.println("elect " + config.electionId());
             } catch (IOException e) {
                 if (running)
                     throw new RuntimeException(e);
+                System.out.println("Error sending election message" + e.getMessage() + " \n " + nextSocket);
             }
         }
     }
 
     protected void declare(int id) {
+        System.out.println("Election Leader: " + id);
         leaderId = id;
         if (id == config.electionId()) {
-            try {
-                Socket socket = new Socket(config.dnsHost(), config.dnsPort());
-                OutputStream out = socket.getOutputStream();
-                BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(socket.getInputStream()));
-                if (!reader.readLine().equals("ok SDP")) {
-                    out.write("Error connecting to DNS Server".getBytes());
-                    out.flush();
-                }
-                out.write(("register " + config.electionDomain() + " " + config.host() + ":" + config.port() + "\n").getBytes());
-                out.flush();
-                if (!reader.readLine().equals("ok")) {
-                    out.write("Error registering with DNS Server".getBytes());
-                    out.flush();
-                }
-                out.close();
-                reader.close();
-                socket.close();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            connectToDNS();
             ping();
         }
         try {
@@ -158,18 +168,55 @@ public class Election implements Runnable {
         }
     }
 
+    private void connectToDNS() {
+        try {
+            System.out.println("Connecting to DNS Server" + config.electionId());
+            Socket socket = new Socket(config.dnsHost(), config.dnsPort());
+            OutputStream out = socket.getOutputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            if (!reader.readLine().equals("ok SDP")) {
+                out.write("Error connecting to DNS Server".getBytes());
+                out.flush();
+            }
+//            write(out,"unregister " + config.electionDomain() + "\n");
+//            System.out.println(reader.readLine());
+
+            out.write(("register " + config.electionDomain() + " " + config.host() + ":" + config.port() + "\n").getBytes());
+            out.flush();
+            if (!reader.readLine().equals("ok")) {
+                out.write("Error registering with DNS Server".getBytes());
+                out.flush();
+            }
+            out.close();
+            reader.close();
+            socket.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     protected void ping() {
         try {
             write(nextSocket.getOutputStream(), "ping\n");
             BufferedReader in = new BufferedReader(new InputStreamReader(nextSocket.getInputStream()));
             if (in.readLine().equals("pong")) {
-                //System.out.println("ping-pong");
                 Thread.sleep(50);
-                return;
             }
-            System.out.println("no pong");
-        } catch (Exception e) {
+            //System.out.println("no pong");
+        } catch (SocketTimeoutException e) {
+            System.err.println(e.getMessage());
+            System.out.println("hier");
+            try {
+                nextSocket.close();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            ringElection(-2);
             throw new RuntimeException(e);
+        } catch (Exception e) {
+            if (running) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -193,7 +240,6 @@ public class Election implements Runnable {
             }
             if (nextSocket != null && !nextSocket.isClosed())
                 nextSocket.close();
-            Thread.currentThread().interrupt();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/dslab/broker/ElectionHandler.java
+++ src/main/java/dslab/broker/ElectionHandler.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 public class ElectionHandler implements Runnable {
     private BufferedReader in;
@@ -19,7 +20,7 @@ public class ElectionHandler implements Runnable {
     private boolean running = true;
 
     public ElectionHandler(BrokerConfig config, Election election, Socket socket) throws IOException {
-        socket.setSoTimeout((int) config.electionHeartbeatTimeoutMs());
+//        socket.setSoTimeout((int) config.electionHeartbeatTimeoutMs());
         this.out = socket.getOutputStream();
         this.election = election;
         this.config = config;
@@ -32,32 +33,28 @@ public class ElectionHandler implements Runnable {
         try {
             String message;
             while ((message = in.readLine()) != null) {
+                System.out.println("Received: " + message);
                 String[] parts = message.split(" ");
                 switch (parts[0]) {
                     case "ping":
-                        //System.out.println("ping");
                         write(out, "pong\n");
                         election.ping();
                         break;
                     case "elect":
+                        System.out.println(message);
                         int id = Integer.parseInt(parts[1]);
-                        //System.out.println("Received election message from " + id);
                         election.ringElection(id);
                         break;
                     case "declare":
                         int elected = Integer.parseInt(parts[1]);
-                        //System.out.println("Received declare message from " + elected);
                         election.declare(elected);
                         break;
                     default:
                         throw new RuntimeException("Unknown message: " + message);
                 }
             }
-            
-            
         } catch (IOException e) {
             throw new RuntimeException(e);
-
         }
     }
 
@@ -66,95 +63,7 @@ public class ElectionHandler implements Runnable {
             out.write(message.getBytes());
             out.flush();
         } catch (IOException e) {
-            System.out.println("write error");
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void ringElection(int id) {
-        if (nextSocket == null) {
-            try {
-                nextSocket = new Socket(config.electionPeerHosts()[0], config.electionPeerPorts()[0]);
-                BufferedReader in = new BufferedReader(new InputStreamReader(nextSocket.getInputStream()));
-                if (in.readLine().equals("ok LEP")) {
-                    write(nextSocket.getOutputStream(), "ok\n");
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        if (id > config.electionId()) {
-            try {
-                //Socket socket = new Socket(config.electionPeerHosts()[0], config.electionPeerPorts()[0]);
-                write(nextSocket.getOutputStream(), "elect " + id + "\n");
-            } catch (IOException e) {
-                System.out.println("Error sending election message");
-                throw new RuntimeException(e);
-            }
-        }
-        if (id == config.electionId()) {
-            try {
-                //Socket socket = new Socket(config.electionPeerHosts()[0], config.electionPeerPorts()[0]);
-                write(nextSocket.getOutputStream(), "declare " + id + "\n");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        if (id < config.electionId()) {
-            try {
-                //Socket socket = new Socket(config.electionPeerHosts()[0], config.electionPeerPorts()[0]);
-                write(nextSocket.getOutputStream(), "elect " + config.electionId() + "\n");
-            } catch (IOException e) {
-                if (running)
-                    throw new RuntimeException(e);
-            }
-        }
-    }
-
-    private void declare(int id) {
-        leaderId = id;
-        if (id == config.electionId()) {
-            try {
-                Socket socket = new Socket(config.dnsHost(), config.dnsPort());
-                OutputStream out = socket.getOutputStream();
-                BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(socket.getInputStream()));
-                if (!reader.readLine().equals("ok SDP")) {
-                    out.write("Error connecting to DNS Server".getBytes());
-                    out.flush();
-                }
-                out.write(("register " + config.electionDomain() + " " + config.host() + ":" + config.port() + "\n").getBytes());
-                out.flush();
-                if (!reader.readLine().equals("ok")) {
-                    out.write("Error registering with DNS Server".getBytes());
-                    out.flush();
-                }
-                out.close();
-                reader.close();
-                socket.close();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-            ping();
-        }
-        try {
-            //Socket socket = new Socket(config.electionPeerHosts()[0], config.electionPeerPorts()[0]);
-            write(nextSocket.getOutputStream(), "declare " + id + "\n");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void ping() {
-        try {
-            write(nextSocket.getOutputStream(), "ping\n");
-            BufferedReader in = new BufferedReader(new InputStreamReader(nextSocket.getInputStream()));
-            if (in.readLine().equals("pong")) {
-                //System.out.println("ping-pong");
-                Thread.sleep(50);
-                return;
-            }
-            System.out.println("no pong");
-        } catch (Exception e) {
+//            System.err.println("write error");
             throw new RuntimeException(e);
         }
     }
