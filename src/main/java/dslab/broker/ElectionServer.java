ec8a82541d25b57d3c5eca34d4a0f74e20bff18f
konstu <konstantin.unterweger@tuwien.ac.at>
2025-01-05 12:21:40 +0100

    ring_becomesLeader_startsSendingHeartbeatsToPeers only one in ring not working

--- src/main/java/dslab/broker/ElectionServer.java
+++ src/main/java/dslab/broker/ElectionServer.java
@@ -64,6 +64,7 @@ public class ElectionServer implements Runnable {
                     }
                     out.write(("elect " + config.electionId() + "\n").getBytes());
                     out.flush();
+                    socket.close();
                     break;
                 } catch (IOException ignored) {}
             }
